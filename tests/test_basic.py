2b708e63d9fce5177da1a197bb5955b53d3b1e3e
Matthias Urlichs <matthias@urlichs.de>
2022-11-10 12:30:26 +0100

    Fix test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,15 +1,29 @@
+import anyio
+import datetime
+from pytz import UTC
+
 from asyncakumuli.mock import Tester
 from asyncakumuli import Entry, DS
 
+from contextlib import aclosing
 
 async def test_basic():
     async with Tester().run() as tc:
-        e = Entry(series="test-A", time=1500000000, value=123, tags={"foo": "one"}, mode=DS.gauge)
+        e = Entry(
+            series="test-A",
+            time=1500000000,
+            value=123,
+            tags={"foo": "one"},
+            mode=DS.gauge,
+        )
         await tc.put(e)
         await anyio.sleep(0.1)
         n = 0
-        async for x in tc.get_data("test-A", tags={}, t_start=1490000000, t_end=1510000000):
-            n += 1
-            assert x[0] == 123
-            assert x[1].timestamp() == 1500000000
+        gd = tc.get_data("test-A", tags={}, t_start=1490000000, t_end=1510000000)
+        async with aclosing(gd):
+            async for x in gd:
+                n += 1
+                assert x.value == 123
+                assert x.time == 1500000000
+                assert x.date == datetime.datetime(2017, 7, 14, 2, 40, tzinfo=UTC)
         assert n == 1
